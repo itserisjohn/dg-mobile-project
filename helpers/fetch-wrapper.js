72866ea37468353d97e1256f8b3a7c986342df8a
itserisjohn <itserisjohn@gmail.com>
2022-08-02 12:41:34 +0800

    helper fix

--- helpers/fetch-wrapper.js
+++ helpers/fetch-wrapper.js
@@ -241,7 +241,7 @@ function handleResponsePublic(response) {
 
 function handleResponsePublicLogin(response) {
   return response.text().then((text) => {
-    const data = text;
+    const data = text && JSON.parse(text);
 
     if (!response.ok) {
       const error = (data && data.message) || response.statusText;
